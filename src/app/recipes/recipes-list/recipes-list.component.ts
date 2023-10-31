d7ea49ae8a7586692c26a3684077abb648d780d9
Wentao Mao <wentaom@foxmail.com>
2023-10-31 14:27:25 +0000

    basic setup

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+})
+export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://picturetherecipe.com/wp-content/uploads/2020/04/PictureTheRecipe-Butter-Chicken.jpg'
+    ),
+  ];
+}
