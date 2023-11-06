12ae15993748eeb08966c3a40512df69c364f5e4
Wentao Mao <wentaom@foxmail.com>
2023-11-06 16:03:15 +0000

    uses router

--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,18 +1,35 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
   styleUrls: ['./recipes-detail.component.css'],
 })
-export class RecipesDetailComponent {
-  @Input() recipe: Recipe;
-  
-  constructor(private recipeService: RecipeService) {}
+export class RecipesDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
+
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(+params['id']);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { RecipeService } from '../../recipe.service';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -8,17 +7,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./recipes-item.component.css'],
 })
 export class RecipesItemComponent {
-  @Input() recipe: {
-    name: string;
-    description: string;
-    imagePath: string;
-    ingredients: Ingredient[];
-  };
+  @Input() recipe: Recipe;
+  @Input() index: number;
 
-  // inject Recipeservice
-  constructor(private recipeService: RecipeService) {}
-
-  onSelectRecipe() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  constructor() {}
 }
